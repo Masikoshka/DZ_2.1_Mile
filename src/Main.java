2b746ccdd4514595457344b8d4ad2ccb99a33223
masikoshka <sheve-masha@yandex.ru>
2021-05-13 23:19:25 +0300

    -v1

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 36_788;
+        int rublesForMile = 20;
+        int bonusMiles = ticketPrice / rublesForMile;
+        System.out.println(bonusMiles);
+    }
+}
